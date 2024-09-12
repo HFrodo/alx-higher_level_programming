8ff2532c356519427f7699d248ee215e1cc773e3
HFrodo <egyepong10@gmail.com>
2024-09-12 05:29:01 +0000

    custom function

---
+++ 0x00-python-hello_world/102-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+print(magic_calculation(1, 2, 3))
+print(magic_calculation(3, 2, 3))
+print(magic_calculation(3, 2, 1))
+print(magic_calculation(5, 5, 5))
+print(magic_calculation(10, 5, 2))
