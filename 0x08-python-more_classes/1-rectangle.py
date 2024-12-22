614af6f6164244d5ac92b5f058c1bb854432d318
hfrodo <root@DAVIDIII>
2024-12-22 15:47:23 +0000

    a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -82,3 +82,12 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self.__height = value
+
+    def __str__(self):
+        '''
+        String representation of the Rectangle object.
+
+        Returns:
+            str: The string representation of the rectangle.
+        '''
+        return f'{{"_Rectangle__height": {self.__height}, "_Rectangle__width": {self.__width}}}'
